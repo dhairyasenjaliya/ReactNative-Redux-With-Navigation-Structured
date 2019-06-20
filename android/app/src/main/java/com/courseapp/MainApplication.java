439d6b0eb8e0a2bbadbc8949cc46f907c80243e0
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2019-06-20 17:58:38 +0530

    Google Sign in

--- android/app/src/main/java/com/courseapp/MainApplication.java
+++ android/app/src/main/java/com/courseapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.courseapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -30,39 +32,13 @@ public class MainApplication extends NavigationApplication {
     return Arrays.<ReactPackage>asList(
             // eg. new VectorIconsPackage()
             new VectorIconsPackage(),
-            new LinearGradientPackage()
+            new LinearGradientPackage(),
+             new RNGoogleSigninPackage(),
+             new RNGestureHandlerPackage()
     );
 
     // No need to add RnnPackage and MainReactPackage
     // Simply return null if you do not have additional packages:
     // return null;
   }
-}
-//public class MainApplication extends Application implements ReactApplication {
-//
-//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-//    @Override
-//    public boolean getUseDeveloperSupport() {
-//      return BuildConfig.DEBUG;
-//    }
-//
-//    @Override
-//    protected List<ReactPackage> getPackages() {
-//      return Arrays.<ReactPackage>asList(
-//          new MainReactPackage(),
- //            new NavigationReactPackage()
-//      );
-//    }
-//  };
-//
-//  @Override
-//  public ReactNativeHost getReactNativeHost() {
-//    return mReactNativeHost;
-//  }
-//
-//  @Override
-//  public void onCreate() {
-//    super.onCreate();
-//    SoLoader.init(this, /* native exopackage */ false);
-//  }
-//}
+} 
--- src/modules/appscreens/FirstScreen.js
+++ src/modules/appscreens/FirstScreen.js
@@ -7,13 +7,97 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as homeActions from '../../actions/homeActions';
 import styles from './styles/FirstScreenStyles';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 
 
 class FirstScreen extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: null,
+			userInfo: '',
+			email: '',
+			password: '',
+			errorMessage: null,
+		};
+	}
+
+	componentDidMount() {
+		GoogleSignin.configure({
+			//It is mandatory to call this method before attempting to call signIn()
+			scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+			// Repleace with your webClientId generated from Firebase console
+			webClientId: '277272543746-asbfl5t12imsf8pdfri4jooabffnqtor.apps.googleusercontent.com',
+		});
+	}
+
+	_signIn = async () => {
+		//Prompts a modal to let the user sign in into your application.
+		try {
+			await GoogleSignin.hasPlayServices({
+				//Check if device has Google Play Services installed.
+				//Always resolves to true on iOS.
+				showPlayServicesUpdateDialog: true,
+			});
+			const userInfo = await GoogleSignin.signIn();
+			console.warn('User Info --> ', userInfo);
+			this.setState({ user: userInfo.user });
+		} catch (error) {
+			console.warn('Message', error.message);
+			if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+				console.warn('User Cancelled the Login Flow');
+			} else if (error.code === statusCodes.IN_PROGRESS) {
+				console.warn('Signing In');
+			} else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+				console.warn('Play Services Not Available or Outdated');
+			} else {
+				console.warn('Some Other Error Happened');
+			}
+		}
+	};
+	_getCurrentUser = async () => {
+		//May be called eg. in the componentDidMount of your main component.
+		//This method returns the current user
+		//if they already signed in and null otherwise.
+		try {
+			const userInfo = await GoogleSignin.signInSilently();
+			this.setState({ userInfo });
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	_signOut = async () => {
+		//Remove user session from the device.
+		try {
+			await GoogleSignin.revokeAccess();
+			await GoogleSignin.signOut();
+			this.setState({ user: null }); // Remove the user from your app's state as well
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	_revokeAccess = async () => {
+		//Remove your application from the user authorized applications.
+		try {
+			await GoogleSignin.revokeAccess();
+			console.warn('deleted');
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	render = () => {
 		return (
 			<View style={styles.container}>
 				<Text>Hollaaaa</Text>
+
+				<GoogleSigninButton
+					style={{ width: 192, height: 40, top: -3 }}
+					size={GoogleSigninButton.Size.Wide}
+					color={GoogleSigninButton.Color.Dark}
+					onPress={this.state.user == null ? this._signIn : this._signOut}
+					disabled={this.state.isSigninInProgress}
+				/>
 			</View>
 		);
 	};
